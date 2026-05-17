cd961ab21d03439b64bf891859ce9c3f29861761
salvagd09 <vasco14_100@hotmail.com>
2026-05-17 00:02:33 -0500

    Cambios en el main para su despliegue

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
-
+import os
+import uvicorn
 from app.core.config import settings
 from app.core.router import api_router
 
@@ -10,3 +11,6 @@ app.include_router(api_router, prefix=settings.API_PREFIX)
 @app.get('/salud', tags=['salud'])
 def salud() -> dict[str, str]:
     return {'estado': 'ok'}
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
